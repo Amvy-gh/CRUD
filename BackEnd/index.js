b1e55db34955f7173122a35f6741d88154665270
Amvy-gh <anjesmanik@gmail.com>
2025-01-20 17:32:27 +0700

    Start Config

---
+++ BackEnd/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import cors from "cors";
+import userRoute from  "./routes/UserRoute.js";
+;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(userRoute);
+
+app.listen(5000, ()=> console.log('listening on port'));
