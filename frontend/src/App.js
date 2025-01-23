6dc0e835297edb59f951afc280939c7c7148c656
Amvy-gh <anjesmanik@gmail.com>
2025-01-24 01:10:37 +0700

    Make an Components of CRUD

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
+import UserList from "./components/UserList";
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import AddUser from "./components/AddUser";
+import EditUser from "./components/EditUser";
+
 function App() {
   return (
-    <div>
-      <h1>HALO</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<UserList />} />
+        <Route path="/add" exact element={<AddUser />} />
+        <Route path="/edit/:id" exact element={<EditUser />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
